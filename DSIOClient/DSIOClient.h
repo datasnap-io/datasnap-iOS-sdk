ee14959a75c6145ac2e4884440f85fecfc0ba284
Mark <mark@datasnap.io>
2015-05-19 11:38:04 -0700

    Initial Commit

---
+++ DSIOClient/DSIOClient.h
@@ -0,0 +1,19 @@
+//
+//  DSIOClient.h
+//  DSIOClient
+//
+//  Created by Mark Watson on 5/19/15.
+//  Copyright (c) 2015 DataSnap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DSIOClient.
+FOUNDATION_EXPORT double DSIOClientVersionNumber;
+
+//! Project version string for DSIOClient.
+FOUNDATION_EXPORT const unsigned char DSIOClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSIOClient/PublicHeader.h>
+
+
