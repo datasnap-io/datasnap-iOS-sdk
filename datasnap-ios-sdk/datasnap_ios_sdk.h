827919e22bc0c173486fdc82294cdfea96076efd
Mark <mark@datasnap.io>
2015-05-21 15:22:23 -0700

    Initial Commit

---
+++ datasnap-ios-sdk/datasnap_ios_sdk.h
@@ -0,0 +1,13 @@
+//
+//  datasnap_ios_sdk.h
+//  datasnap-ios-sdk
+//
+//  Created by Mark Watson on 5/21/15.
+//  Copyright (c) 2015 DataSnap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface datasnap_ios_sdk : NSObject
+
+@end
