408736b7866b43e74b2956770ad57f9b06c60fee
Layne McIntyre <amcintyre@grio.com>
2016-07-06 16:04:45 -0700

    Adding comments

--- Example/Datasnap/DSIOAppDelegate.m
+++ Example/Datasnap/DSIOAppDelegate.m
@@ -17,7 +17,7 @@
     [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
     [[UIApplication sharedApplication] registerForRemoteNotifications];
 
-    VendorProperties* vendorProperties = [[VendorProperties alloc] initWithVendor:GIMBAL andgimbalApiKey:@"74e344e9-9625-4b9d-96cf-e7805479d33c"];
+    //VendorProperties* vendorProperties = [[VendorProperties alloc] initWithVendor:GIMBAL andgimbalApiKey:@"74e344e9-9625-4b9d-96cf-e7805479d33c"];
     Datasnap* datasnap = [Datasnap sharedClient];
     //IDFAOptIn is a property that, when true, allows Datasnap to collect the IDFA for the device. If YES is passed for this parameter, ads must be served through the application. If not, the App Store will not accept the app.
     //Provided email will be hashed before being sent to Datasnap server.
@@ -27,14 +27,15 @@
                               projectId:@"21213f8b-8341-4ef3-a6b8-ed0f84945186"
                               IDFAOptIn:YES
                                   email:@"someemail@example.com"
-                    andVendorProperties:vendorProperties];
-    [datasnap setFlushParamsWithDuration:100 withMaxElements:10];
+                    andVendorProperties:nil];
+    //max elements property denotes the batch size, duration property denotes the amount of milliseconds the sdk will wait before making an api call
+    [datasnap setFlushParamsWithDuration:10000 withMaxElements:10];
     return YES;
 }
 - (void)application:(UIApplication*)app
     didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
 {
-    [Gimbal setPushDeviceToken:deviceToken];
+   // [Gimbal setPushDeviceToken:deviceToken];
 }
 
 - (void)application:(UIApplication*)app
@@ -44,24 +45,18 @@
 }
 - (void)applicationWillResignActive:(UIApplication*)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication*)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication*)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication*)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication*)application
