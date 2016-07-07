6e870c49f801801e1c255a72c2a251883f46cdd5
Layne McIntyre <amcintyre@grio.com>
2016-07-07 15:24:52 -0700

    Adding placeholders

--- Example/Datasnap/DSIOAppDelegate.m
+++ Example/Datasnap/DSIOAppDelegate.m
@@ -17,17 +17,17 @@
     [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
     [[UIApplication sharedApplication] registerForRemoteNotifications];
 
-    //VendorProperties* vendorProperties = [[VendorProperties alloc] initWithVendor:GIMBAL andgimbalApiKey:@"74e344e9-9625-4b9d-96cf-e7805479d33c"];
+    VendorProperties* vendorProperties = [[VendorProperties alloc] initWithVendor:GIMBAL andgimbalApiKey:@"Enter your Gimbal API key"];
     Datasnap* datasnap = [Datasnap sharedClient];
     //IDFAOptIn is a property that, when true, allows Datasnap to collect the IDFA for the device. If YES is passed for this parameter, ads must be served through the application. If not, the App Store will not accept the app.
     //Provided email will be hashed before being sent to Datasnap server.
-    datasnap = [datasnap initWithApiKey:@"3F34FXD78PCINFR99IYW950W4"
-                           apiKeySecret:@"KA0HdzrZzNjvUq8OnKQoxaReyUayZY0ckNYoMZURxK8"
-                         organizationId:@"19CYxNMSQvfnnMf1QS4b3Z"
-                              projectId:@"21213f8b-8341-4ef3-a6b8-ed0f84945186"
+    datasnap = [datasnap initWithApiKey:@"Enter your Datasnap API key"
+                           apiKeySecret:@"Enter your Datasnap API secret"
+                         organizationId:@"Enter your organization ID"
+                              projectId:@"Enter your project ID"
                               IDFAOptIn:YES
-                                  email:@"someemail@example.com"
-                    andVendorProperties:nil];
+                                  email:@"someone@example.com"
+                    andVendorProperties:vendorProperties];
     //max elements property denotes the batch size, duration property denotes the amount of milliseconds the sdk will wait before making an api call
     [datasnap setFlushParamsWithDuration:10000 withMaxElements:10];
     return YES;
@@ -35,7 +35,7 @@
 - (void)application:(UIApplication*)app
     didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
 {
-   // [Gimbal setPushDeviceToken:deviceToken];
+    [Gimbal setPushDeviceToken:deviceToken];
 }
 
 - (void)application:(UIApplication*)app
